1cc2972ca590b0219c111d4a8963940fb0e22563
Andy Arijs <78924184+Dj-Shortcut@users.noreply.github.com>
2026-02-08 19:21:38 +0100

    Update events.ts

--- src/events.ts
+++ src/events.ts
@@ -1,6 +1,14 @@
 export type InternalEvent = {
   userId: string;
-  type: "post" | "comment";
+  type:
+    | "post"
+    | "comment"
+    | "message"
+    | "message_read";
   timestamp: number;
   source: "facebook" | "test" | "cli";
+
+  // optioneel, future-proof
+  targetId?: string;
+  meta?: Record<string, any>;
 };
