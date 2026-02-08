cc590e040ab60f19cda537dbcd10ecfc545d1203
Andy Arijs <78924184+Dj-Shortcut@users.noreply.github.com>
2026-02-08 20:20:41 +0100

    Update handleEvent.ts

--- src/handleEvent.ts
+++ src/handleEvent.ts
@@ -1,11 +1,21 @@
+import { InternalEvent, isScorableEvent } from "./events.js";
 import { awardPoints } from "./xpEngine.js";
-import { InternalEvent } from "./events.js";
 
-export function handleEvent(event: InternalEvent, debug = false) {
-  return awardPoints(
-    event.userId,
-    event.type,
-    event.timestamp,
-    debug
-  );
+/**
+ * Centrale event handler
+ * - ontvangt ALLE events
+ * - beslist of ze scorable zijn
+ */
+export function handleEvent(event: InternalEvent) {
+  if (isScorableEvent(event.type)) {
+    return awardPoints(
+      event.userId,
+      event.type,
+      event.timestamp
+    );
+  }
+
+  // Niet-scorable events (message, message_read, …)
+  // zijn geldig maar leveren geen punten op
+  return { ok: true };
 }
